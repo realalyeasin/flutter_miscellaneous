d4dbede31a4d6f2bea9c117cf8be67822d5b1a22
Yeasin <realalyeasin@gmail.com>
2022-02-03 18:35:36 +0600

    flutter_miscellaneous

--- lib/FilterSearch/FilterSearch.dart
+++ lib/FilterSearch/FilterSearch.dart
@@ -5,10 +5,15 @@ import 'package:flutter/material.dart';
 class FilterSearch extends StatefulWidget {
   const FilterSearch({Key? key}) : super(key: key);
 
+
   @override
   _FilterSearchState createState() => _FilterSearchState();
 }
-
+class Word{
+  final String? name;
+  final String? mean;
+  Word(this.name, this.mean);
+}
 class _FilterSearchState extends State<FilterSearch> {
 
   final c = [
@@ -20,16 +25,16 @@ class _FilterSearchState extends State<FilterSearch> {
     Colors.white
   ];
   final List<Map<String, dynamic>> _allWords = [
-    {'id' : 1, 'name' : 'Apaling', 'mean' : 'Fear'},
-    {'id' : 2, 'name' : 'Indulgent', 'mean' : 'Lenient'},
-    {'id' : 3, 'name' : 'Degenerate', 'mean' : 'Degrade'},
-    {'id' : 4, 'name' : 'Persist', 'mean' : 'Persevere'},
-    {'id' : 5, 'name' : 'Anathema', 'mean' : 'Evil/Cursed'},
-    {'id' : 6, 'name' : 'Flourish', 'mean' : 'Grow vigorously'},
-    {'id' : 7, 'name' : 'Redundancy', 'mean' : 'Layoff'},
-    {'id' : 8, 'name' : 'Complacence', 'mean' : 'Self-Satisfaction'},
-    {'id' : 9, 'name' : 'Blatant', 'mean' : 'Unpleasantly'},
-    {'id' : 10, 'name' : 'Pungent', 'mean' : 'Rancid'},
+    {'id' : 1, 'name' : 'Abate', 'mean' : 'v. to diminish in intensity'},
+    {'id' : 2, 'name' : 'Aberrant', 'mean' : 'adj. diverging from the standard type'},
+    {'id' : 3, 'name' : 'Abjure', 'mean' : 'v. to reject or renounce'},
+    {'id' : 4, 'name' : 'Abscond', 'mean' : 'v. to leave secretly, evading detection'},
+    {'id' : 5, 'name' : 'Abstain', 'mean' : 'v. to voluntarily refrain from doing something'},
+    {'id' : 6, 'name' : 'Acumen', 'mean' : 'n. keen judgment and perception'},
+    {'id' : 7, 'name' : 'Admonish', 'mean' : 'v. scold or to advise firmly'},
+    {'id' : 8, 'name' : 'Adulterate', 'mean' : 'v. to contaminate or make impure by introducing inferior elements'},
+    {'id' : 9, 'name' : 'Advocate', 'mean' : 'v. to recommend, support, or advise'},
+    {'id' : 10, 'name' : 'Aesthetic', 'mean' : 'adj. concerned with the nature of beauty and art'},
   ];
 
   List<Map<String, dynamic>> _foundWords = [];
@@ -52,10 +57,11 @@ class _FilterSearchState extends State<FilterSearch> {
     _foundWords = results;
   });
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.redAccent.shade200,
+      backgroundColor: Colors.orange.shade200,
       appBar: AppBar(
         backgroundColor: Colors.black,
         title: Text('GRE Important Vocabs',
@@ -71,9 +77,9 @@ class _FilterSearchState extends State<FilterSearch> {
               onChanged: (value) => _runFilter(value),
               cursorColor: Colors.white,
               decoration: InputDecoration(
-                labelText: 'Search', labelStyle: TextStyle(color: Colors.white, letterSpacing: 3, fontWeight: FontWeight.w600, fontSize: 19),
+                labelText: 'Search', labelStyle: TextStyle(color: Colors.white,  letterSpacing: 3, fontWeight: FontWeight.bold, fontSize: 19),
                 suffixIcon: Icon(Icons.search, color: Colors.white,),
-                icon: IconButton(icon: Icon(Icons.list_alt, color: Colors.black, size: 35,), onPressed: () {  },)
+                icon: IconButton(icon: Icon(Icons.list_alt, color: Colors.white, size: 35,), onPressed: () {  },)
               ),
             ),
             SizedBox(height: 5,),
@@ -85,9 +91,10 @@ class _FilterSearchState extends State<FilterSearch> {
                   elevation: 4,
                   margin: EdgeInsets.symmetric(vertical: 10),
                   child: ListTile(
-                    leading: Icon(Icons.circle, color: c[Random().nextInt(4)],),
-                    title: Text(_foundWords[index]['name']),
-                    subtitle: Text(_foundWords[index]['mean']),
+                    leading: Icon(Icons.stacked_line_chart, color: Colors.blue),
+                    title: Text(_foundWords[index]['name'], style: TextStyle(fontWeight: FontWeight.bold, letterSpacing: 1, fontSize: 18),),
+                    subtitle: Text(_foundWords[index]['mean'], style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black, letterSpacing: 1),),
+                    trailing: IconButton(icon: Icon(Icons.call_made_outlined, color: Colors.black,), onPressed: () {  },),
                   ),
 
                 )) : Text('Nothing Found'))
