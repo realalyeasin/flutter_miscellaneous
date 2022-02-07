2ee38e663e4f3a73bb8c50bbec44167683404d3b
Yeasin <realalyeasin@gmail.com>
2022-02-07 21:53:00 +0600

    flutter_miscellaneous

--- lib/SliverBar/SliverBar.dart
+++ lib/SliverBar/SliverBar.dart
@@ -14,116 +14,128 @@ class _SliverBarState extends State<SliverBar> {
   final List _gridItems = List.generate(81, (i) => 'Item $i');
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.black,
-      appBar: AppBar(
-        title: Text('Sliver Bar'),
-        backgroundColor: Colors.black26,
-        automaticallyImplyLeading: true,
-      ),
-      body: CustomScrollView(
-        slivers: [
-          SliverAppBar(
-            automaticallyImplyLeading: false,
-            pinned: true,
-            backgroundColor: Colors.teal,
-            elevation: 1,
-            expandedHeight: 200.0,
-            flexibleSpace: FlexibleSpaceBar(
-              background: Container(
-                margin: EdgeInsets.all(10),
-                color: Colors.lightBlueAccent,
-                child: Center(
-                  child: Icon(Icons.filter_list_outlined, size: 60, color: Colors.black,),
+    return Scrollbar(
+      isAlwaysShown: true,
+      showTrackOnHover: true,
+      scrollbarOrientation: ScrollbarOrientation.right,
+      interactive: true,
+      thickness: 6,
+      hoverThickness: 10,
+      radius: Radius.circular(0),
+      notificationPredicate: (ScrollNotification notification){
+        return notification.depth==0;
+      },
+      child: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          title: Text('Sliver Bar'),
+          backgroundColor: Colors.black26,
+          automaticallyImplyLeading: true,
+        ),
+        body: CustomScrollView(
+          slivers: [
+            SliverAppBar(
+              automaticallyImplyLeading: false,
+              pinned: true,
+              backgroundColor: Colors.teal,
+              elevation: 1,
+              expandedHeight: 200.0,
+              flexibleSpace: FlexibleSpaceBar(
+                background: Container(
+                  margin: EdgeInsets.all(10),
+                  color: Colors.lightBlueAccent,
+                  child: Center(
+                    child: Icon(Icons.filter_list_outlined, size: 60, color: Colors.black,),
+                  ),
                 ),
+                title: Text('First Sliver', style: TextStyle(color: Colors.black, letterSpacing: 2,fontWeight: FontWeight.bold, fontSize: 15, ),),
+                centerTitle: true,
               ),
-              title: Text('First Sliver', style: TextStyle(color: Colors.black, letterSpacing: 2,fontWeight: FontWeight.bold, fontSize: 15, ),),
-              centerTitle: true,
             ),
-          ),
-          SliverGrid.count(
-            crossAxisCount: 2,
-            mainAxisSpacing: 20.0,
-            crossAxisSpacing: 20.0,
-            childAspectRatio: 1,
-            children: [
-              Card(
-                shadowColor: Colors.white,
-                margin: EdgeInsets.all(12),
-                color: Colors.orangeAccent,
-                child: Container(),
+            SliverGrid.count(
+              crossAxisCount: 2,
+              mainAxisSpacing: 20.0,
+              crossAxisSpacing: 20.0,
+              childAspectRatio: 1,
+              children: [
+                Card(
+                  shadowColor: Colors.white,
+                  margin: EdgeInsets.all(12),
+                  color: Colors.orangeAccent,
+                  child: Container(),
+                ),
+                Card(
+                  shadowColor: Colors.white,
+                  margin: EdgeInsets.all(12),
+                  color: Colors.orangeAccent,
+                  child: Container(),
+                ),
+                Card(
+                  shadowColor: Colors.white,
+                  margin: EdgeInsets.all(12),
+                  color: Colors.orangeAccent,
+                  child: Container(),
+                ),
+                Card(
+                  shadowColor: Colors.white,
+                  margin: EdgeInsets.all(12),
+                  color: Colors.orangeAccent,
+                  child: Container(),
+                ),
+                Card(
+                  shadowColor: Colors.white,
+                  margin: EdgeInsets.all(12),
+                  color: Colors.orangeAccent,
+                  child: Container(),
+                ),
+                Card(
+                  shadowColor: Colors.white,
+                  margin: EdgeInsets.all(12),
+                  color: Colors.orangeAccent,
+                  child: Container(),
+                )
+              ],),
+            SliverPadding(padding: EdgeInsets.symmetric(vertical: 10)),
+            SliverAppBar(
+              automaticallyImplyLeading: false,
+              elevation: 5,
+              pinned: true,
+              backgroundColor: Colors.pink,
+              expandedHeight: 300.0,
+              flexibleSpace: FlexibleSpaceBar(
+                centerTitle: true,
+                background: Container(
+                  margin: EdgeInsets.all(10.0,),
+                  color: Colors.yellow,
+                  child: Icon(Icons.book, color: Colors.black,size: 60,),
+                ),
+                title: Text('Second Sliver',style: TextStyle(color: Colors.black, letterSpacing: 2,fontWeight: FontWeight.bold, fontSize: 15, )),
               ),
-              Card(
-                shadowColor: Colors.white,
-                margin: EdgeInsets.all(12),
-                color: Colors.orangeAccent,
-                child: Container(),
-              ),
-              Card(
-                shadowColor: Colors.white,
-                margin: EdgeInsets.all(12),
-                color: Colors.orangeAccent,
-                child: Container(),
-              ),
-              Card(
-                shadowColor: Colors.white,
-                margin: EdgeInsets.all(12),
-                color: Colors.orangeAccent,
-                child: Container(),
-              ),
-              Card(
-                shadowColor: Colors.white,
-                margin: EdgeInsets.all(12),
-                color: Colors.orangeAccent,
-                child: Container(),
-              ),
-              Card(
-                shadowColor: Colors.white,
-                margin: EdgeInsets.all(12),
-                color: Colors.orangeAccent,
-                child: Container(),
-              )
-            ],),
-          SliverPadding(padding: EdgeInsets.symmetric(vertical: 10)),
-          SliverAppBar(
-            automaticallyImplyLeading: false,
-            elevation: 5,
-            pinned: true,
-            backgroundColor: Colors.pink,
-            expandedHeight: 300.0,
-            flexibleSpace: FlexibleSpaceBar(
-              centerTitle: true,
-              background: Container(
-                margin: EdgeInsets.all(10.0,),
-                color: Colors.yellow,
-                child: Icon(Icons.book, color: Colors.black,size: 60,),
-              ),
-              title: Text('Second Sliver',style: TextStyle(color: Colors.black, letterSpacing: 2,fontWeight: FontWeight.bold, fontSize: 15, )),
             ),
-          ),
-          SliverPadding(padding: EdgeInsets.symmetric(vertical: 20)),
-          SliverGrid(
-              delegate: SliverChildBuilderDelegate(
-                  (context, index){
-                    return Card(
-                      color: Colors.teal[Random().nextInt(9)*100],
-                      child: Container(
-                        alignment: Alignment.center,
-                        child: Text(_gridItems[index]),
-                      ),
-                    );
-                  },
-                childCount: _gridItems.length
-              ),
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: 2,
-                mainAxisSpacing: 10,
-                crossAxisSpacing: 10,
-                childAspectRatio: 2
-              ),
-          ),
-          SliverPadding(padding: EdgeInsets.symmetric(vertical: 20)),
-        ],
+            SliverPadding(padding: EdgeInsets.symmetric(vertical: 20)),
+            SliverGrid(
+                delegate: SliverChildBuilderDelegate(
+                    (context, index){
+                      return Card(
+                        color: Colors.teal[Random().nextInt(9)*100],
+                        child: Container(
+                          alignment: Alignment.center,
+                          child: Text(_gridItems[index]),
+                        ),
+                      );
+                    },
+                  childCount: _gridItems.length
+                ),
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 10,
+                  crossAxisSpacing: 10,
+                  childAspectRatio: 2
+                ),
+            ),
+            SliverPadding(padding: EdgeInsets.symmetric(vertical: 20)),
+          ],
+        ),
       ),
     );
   }
